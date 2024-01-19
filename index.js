34c1d877e2675fea8490c92094327bfb83542416
jrealiquez-2019342 <jrealiquez-2019342@kinal.edu.gt>
2024-01-19 09:23:25 -0600

    Avances en el Java Script

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', function restar() {
     const canva = document.getElementById("myCanvas");
     const contexto = canva.getContext('2d');
-    
-    var palabras = ["COMPUTADORA", "CPU", "MOUSE"];
-    var numeroAleatorio = Math.floor(Math.random*palabras.length)
-
     cargarJuego();
 })
 
+const diccionario = [["COMPUTADORA", "TODO PROGRAMADOR LA UTILIZA PARA TRABAJAR"], ["CPU", "LOGICA MATEMATICA DE LA PC"], ["MOUSE", "NOS AYUDA A MOVER EL CURSOR"]];
+var numeroAleatorio = Math.floor(Math.random()*diccionario.length);
+var palabraJuego;
+
 function cargarJuego(){
-    window.alert("Juego cargado");
-
-}
-
-function obtenerPalabra() {
-    
+    palabraJuego = diccionario[numeroAleatorio];
+    console.log(palabraJuego[0]);
+    dibujarEstructura();
 }
 
 function cargarBotones() {
     
+}
+
+function dibujarEstructura( ){
+    contexto.beginPath();
+    contexto.moveTo(50,50);
+    contexto.lineTo(200,100,)
 }
